ed78651ea2fff6617f42868e44ec130ed4f97aa2
JUG_UK <81421985+JUGUK@users.noreply.github.com>
2021-07-02 09:22:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+
+// Here is the code to make a bot react to some Keywords by text.
+
+          
+ 
+  	// Fist category example.
+  
+      client.on('message', message => { 
+  
+        if(message.content.toLowerCase() === 'Keyword')
+            message.channel.send('<emoteID>・<emoteID> This is an exemple of the text will appear on BOT response, __**exemple**__, __**exemple**__, __**exemple**__ ou la __**exemple**__');
+        
+            else if(message.content.toLowerCase() === 'Keyword')
+            message.channel.send('<emoteID>・<emoteID> This is an exemple of the text will appear on BOT response, __**exemple**__, __**exemple**__, __**exemple**__ ou la __**exemple**__');
+        
+           
+       
+      // more exemple after this line, not required
+      
+      
+      
+       //annother category bellow
+        
+        
+            else if(message.content.toLowerCase() === 'Keyword')
+            message.channel.send('<emoteID> ・<emoteID>  This is an exemple of the text will appear on BOT response <emoteID> ');
+        
+            else if(message.content.toLowerCase() === 'Keyword')
+            message.channel.send('<emoteID> ・<emoteID>  This is an exemple of the text will appear on BOT response <emoteID> ');
+      
+      
+          })
+      
